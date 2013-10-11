9ef46a1ddbf54717e6a717cee6633ca919e59fd5
jefjef <jeffrey.smets@gmail.com>
2013-10-11 14:58:05 +0200

    no message

--- NetBeansProjects/DesKundig/src/deskundig/Runner.java
+++ NetBeansProjects/DesKundig/src/deskundig/Runner.java
@@ -2,11 +2,16 @@ package deskundig;
 
 public class Runner{
     public static void main (String[] args){
-        String tekst = "Dit is een tekst";
-        String sleutel = "eensleutel";
+        String tekst = "Dit is een andere tekst, hihi";
+        String[] sleutels = {"eerstesleutel", "tweedesleutel", "derdesleutel"};
         
-        Encryptie en = new Encryptie(tekst, sleutel);
-        String done = en.Encrypteer(false);  
+        Encryptie en = new Encryptie(sleutels);
+        String done = en.Encrypteer(tekst);  
         System.out.println(done);
+        
+        Encryptie den = new Encryptie(sleutels);
+        String decDone = den.Decrypteer(done);
+        System.out.println(decDone);
+        
     }
 }
